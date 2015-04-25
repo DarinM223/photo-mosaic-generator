7cfe5fb81496761be5293d0ecaeae6facf029d24
Darin Minamoto <darinm223@gmail.com>
2015-04-25 04:23:04 -0700

    Started on image processing

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"image"
+	"image/color"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -128,6 +130,53 @@ func RetrieveImageData(tag string, access_token string) (TagSearchResponse, erro
 	}
 
 	return tagSearchResponse, nil
+}
+
+func RetrieveImages(tagSearchResponse TagSearchResponse, c chan<- *image.Image) {
+	go func() {
+		for _, value := range tagSearchResponse.Data {
+			resp, err := http.Get(value.Images.Thumbnail.Url)
+			defer resp.Body.Close()
+			if err != nil {
+				c <- nil
+			} else {
+				img, _, err := image.Decode(resp.Body)
+				if err != nil {
+					c <- &img
+				} else {
+					c <- nil
+				}
+			}
+
+		}
+	}()
+}
+
+// TODO: implement this
+func CalculateAverageColor(img *image.Image) color.Color {
+	return (*img).At(0, 0)
+}
+
+/// receive images as they are sent and calculate average color for each image
+func ProcessImages(imagesLength int, c <-chan *image.Image, retChan chan<- map[color.Color]*image.Image) {
+	index := 0
+	imageMap := make(map[color.Color]*image.Image)
+	for {
+		img := <-c
+		if img != nil {
+			// calculate the average color
+			c := CalculateAverageColor(img)
+			if imageMap[c] != nil {
+				imageMap[c] = img
+			}
+		}
+		index++
+		// send return map after processing all of the images
+		if index >= imagesLength {
+			retChan <- imageMap
+			break
+		}
+	}
 }
 
 func main() {
